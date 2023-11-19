a311ce794a0c1135c9b0e8cf84d8e6d0f6584abb
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2023-11-19 06:52:41 +0500

    fixed issue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,8 @@
-import { ApplicationConfig, ErrorHandler } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { GlobalErrorHandler } from '../global-error-handler';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { errorInterceptorInterceptor } from './error-interceptor.interceptor';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimations()],
