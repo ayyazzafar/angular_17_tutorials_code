3a4d45a76194f91df8ac8c4b3f2dd5b6c7670b29
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2024-01-20 14:37:32 +0500

    Add star ratings with different values

--- src/app/star-rating/star-rating.component.ts
+++ src/app/star-rating/star-rating.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
@@ -13,9 +13,11 @@ import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
 export class StarRatingComponent {
   faStar = faStar;
 
-  rating = 0;
+  @Input() rating: number = 0;
+  @Input() readonly: boolean = false;
 
   setRating(value: number) {
+    if (this.readonly) return;
     this.rating = value;
   }
 }
