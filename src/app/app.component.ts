d61d2af65c4fca15a173209921f6e7a17477588c
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2024-01-22 15:26:36 +0500

    Add fixed navbar with scroll event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { fromEvent, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    fromEvent(window, 'scroll')
+      .pipe(throttleTime(100))
+      .subscribe((event) => this.onWindowScroll(event));
+  }
+
+  onWindowScroll(event: Event) {
+    console.log('scrolling');
+    // Logic to handle scroll events will be here
+    let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    scrollPosition = scrollPosition + 160;
+
+    const sections: any = document.querySelectorAll('.section');
+
+    sections.forEach((section: HTMLElement) => {
+      if (
+        section.offsetTop <= scrollPosition &&
+        section.offsetTop + section.offsetHeight > scrollPosition
+      ) {
+        console.log(section.id);
+        let navLinks: any = document.querySelectorAll('.navbar a');
+        navLinks.forEach((link: HTMLAnchorElement) => {
+          if (link.href.includes(section.id)) {
+            link.classList.add('active');
+          } else {
+            link.classList.remove('active');
+          }
+        });
+      }
+    });
+  }
+}
