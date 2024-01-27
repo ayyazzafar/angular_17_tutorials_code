13bab71d86ee7b75df889e00de43ba1f2fbf581c
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2024-01-27 12:00:29 +0500

    Add spinner component to show loading state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  isLoading = true;
+
+  constructor() {
+    // setTimeout(() => {
+    //   this.isLoading = false;
+    // }, 3000);
+  }
+}
---
+++ src/app/spinner/spinner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './spinner.component.html',
+  styleUrl: './spinner.component.scss',
+})
+export class SpinnerComponent {}
