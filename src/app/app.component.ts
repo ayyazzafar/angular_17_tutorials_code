44607f39a468187cac4a933bd241b3d3b902a2d8
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2024-02-02 13:23:30 +0500

    Add user profile form with personal information and addresses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  fb = inject(FormBuilder);
+
+  userProfileForm = this.fb.group({
+    personalInfo: this.fb.group({
+      name: '',
+      email: '',
+    }),
+    addresses: this.fb.array([
+      this.fb.group({
+        street: '',
+        city: '',
+        zipCode: '',
+      }),
+    ]),
+  });
+
+  get addressGroups() {
+    return this.userProfileForm.get('addresses') as FormArray;
+  }
+
+  addAddress() {
+    this.addressGroups.push(
+      this.fb.group({
+        street: '',
+        city: '',
+        zipCode: '',
+      })
+    );
+  }
+
+  removeAddress(index: number) {
+    this.addressGroups.removeAt(index);
+  }
+}
