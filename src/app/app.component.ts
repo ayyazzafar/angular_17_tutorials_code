7bf31cdd52f7862f488198f461c3f8350d192233
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2024-02-03 12:00:11 +0500

    Add Swiper library and implement autoplay feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +12,22 @@ import { RouterOutlet } from '@angular/router';
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const progressCircle: any = document.querySelector(
+      '.autoplay-progress svg'
+    );
+    const progressContent: any = document.querySelector(
+      '.autoplay-progress span'
+    );
+
+    const swiperEl: any = document.querySelector('swiper-container');
+    swiperEl.addEventListener('autoplaytimeleft', (e: any) => {
+      const [swiper, time, progress] = e.detail;
+      progressCircle.style.setProperty('--progress', 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerSwiperElements();
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
